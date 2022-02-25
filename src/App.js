de9c4d970f493f927911e379e1066776fbcd7937
Brandon Denney <ee.brandon@gmail.com>
2022-02-25 15:59:27 -0500

    Created patient form

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import Header from './components/Header';
+import NewPatientForm from './components/NewPatientForm';
 import PatientList from './components/PatientList';
 
 function App() {
   return (
     <div className="root">
       <Header/>
+      <NewPatientForm />
       <PatientList />
     </div>
   );
---
+++ src/components/NewPatientForm.js
@@ -0,0 +1,22 @@
+import React from "react"
+
+function NewPatientForm() {
+    return(
+        <>
+            <form id="new-patient-form">
+                <input type="text" placeholder="Patient Name" />
+                <select name="side-effects" id="side-effects" form="new-patient-form">
+                    <option value="dizziness">Dizziness</option>
+                    <option value="nausea">Nausea</option>
+                    <option value="somnambulism">Somnambulism</option>
+                    <option value="memory">Memory</option>
+                    <option value="allergy">Severe Allergic Reaction</option>
+                    <option value="headache">Headache</option>
+                </select>
+                <input type="submit" value="Add Patient" />
+            </form>
+        </>
+    )
+}
+
+export default NewPatientForm;
