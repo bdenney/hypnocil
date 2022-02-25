57dec4a11a0a2931106b5105587dee51c69f797e
Brandon Denney <ee.brandon@gmail.com>
2022-02-25 18:18:44 -0500

    Added basic styling.

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,6 +3,7 @@ import React from "react"
 function Header() {
     return(
         <header>
+            <img src="./images/hypnocil-logo.png"/>
             <h1>Hypnocil™️ Clinical Trials</h1>
             <input id="search" type="text" placeholder="Search..."></input>
         </header>
