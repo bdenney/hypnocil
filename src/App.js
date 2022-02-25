aa064ef4ee57ad94fab908a0e69723ee5313bfb9
Brandon Denney <ee.brandon@gmail.com>
2022-02-25 15:00:46 -0500

    Created PatientList component.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Header from './components/Header';
+import PatientList from './components/PatientList';
 
 function App() {
   return (
     <div class="root">
       <Header/>
+      <PatientList />
     </div>
   );
 }
---
+++ src/components/PatientList.js
@@ -0,0 +1,17 @@
+import React from "react"
+
+function PatientList() {
+    return(
+      <table>
+          <thead>
+              <th></th>
+              <th>Patient ID</th>
+              <th>Patient Name</th>
+              <th>Noted Side Effects</th>
+          </thead>
+          {/* Create <Patient/> components here. */}
+      </table>  
+    );
+}
+
+export default PatientList;
